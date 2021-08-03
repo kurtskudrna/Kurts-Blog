b28409e3ed6a1e026418b1692285f53fb389ce8b
Kurt Skudrna <kurtskudrna@gmail.com>
2021-08-02 23:26:07 -0400

    create post page started and base.html set

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Main, Detail
+from .views import Main, Detail, CreatePost
 
 urlpatterns = [
     path('', Main.as_view(), name='main'),
     path('post/<int:pk>', Detail.as_view(), name='detail'),
+    path('create_post', CreatePost.as_view(), name='create_post')
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,8 +1,8 @@
 from myblog.models import Post
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
-# List brings multiple records to list out, Detail will bring a single
+# List brings multiple records to list out, CreateView creates a view, Detail will bring a single
 
 
 class Main(ListView):
@@ -13,3 +13,9 @@ class Main(ListView):
 class Detail(DetailView):
     model = Post
     template_name = 'detail.html'
+
+
+class CreatePost(CreateView):
+    model = Post
+    template_name = 'create_post.html'
+    fields = '__all__'
